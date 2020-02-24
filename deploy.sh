e95a3ac406507df51aa0e735105e24519ec19fba
Rachit Nigam <rachit.nigam12@gmail.com>
2020-02-24 14:13:15 -0500

    update script

--- deploy.sh
+++ deploy.sh
@@ -14,28 +14,8 @@ fi
 echo "Saving unsaved changes..."
 git stash
 
-# Start hugo server to checklinks.
-#if hash linkchecker; then
-  #echo "linkchecker found, checking links."
-
-  ## If a hugo server is already running, don't run another one.
-  #if [ "$(pgrep hugo)" -eq "" ]; then
-    #hugo server &
-    #SERVER_PID="$!"
-    #linkchecker http://localhost:1313 --check-extern
-    #kill "${SERVER_PID}"
-  #else
-    #linkchecker http://localhost:1313 --check-extern
-  #fi
-#else
-  #echo "linkchecker not found, skipping link checks"
-#fi
-
-# Remove existing public folder.
-rm -rf public/
-
 # Build the website.
-hugo
+hugo49.2
 
 # If purify-css is installed as an exec, purify bootstrap.min.css
 which purifycss > /dev/null
@@ -55,22 +35,11 @@ if [ $? -eq 0 ]; then
 fi
 
 # Move public/ to docs/ for github pages deployment.
-rm -rf docs/
-mv public/ docs/
-git checkout docs/CNAME
+cp CNAME public/
 
-if [[ "$website" != "" ]]; then
-  # Push the changes to the remote server
-  rsync -a --delete-excluded docs/ "$website"
-
-  if [ "$?" -ne 0 ]; then
-      echo "Failed to update the remote website server!"
-  fi
-else
-  echo "No website configured."
-fi
-
-echo "Commit the docs/ folder to deploy to github pages."
-
-echo "Restoring unsaved changes"
 git stash pop
+
+cd public/
+git add -A
+git commit -m 'update'
+git push
